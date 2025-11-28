551d405d7e419ecdb3f4bb8897e9c0c5b4c01ce3
Tommy <czambrano@fi.uba.ar>
2025-11-28 00:39:42 -0300

    Agregué la lógica para visualizar los cambios de editar en vista-previa

--- frontend/editar-perfil.js
+++ frontend/editar-perfil.js
@@ -2,45 +2,37 @@ const inputNombre = document.getElementById('editar-nombre');
 const inputBio = document.getElementById('editar-bio');
 const inputGenero = document.getElementById('editar-genero');
 const inputUbicacion = document.getElementById('editar-ubicacion');
+const inputEdad = document.getElementById('editar-edad');
+const inputOrientacion = document.getElementById('editar-orientacion-sexual');
 
 const vistaPreviaNombre = document.getElementById('vista-previa-nombre');
 const vistaPreviaBio = document.getElementById('vista-previa-bio');
 const vistaPreviaGenero = document.getElementById('vista-previa-genero');
 const vistaPreviaUbicacion = document.getElementById('vista-previa-ciudad');
+const vistaPreviaEdad = document.getElementById('vista-previa-edad');
+const vistaPreviaOrientacion = document.getElementById('vista-previa-orientacion');
 
-inputNombre.addEventListener('input', () => {
-    const nuevoNombre = inputNombre.value.trim();
-    if (nuevoNombre) {
-        vistaPreviaNombre.textContent = nuevoNombre;
-    } else {
-        vistaPreviaNombre.textContent = 'Nombre Apellido';
-    }
-});
+function mostrarVistaPrevia(editarElemento, vistaPreviaElemento, textoDefault, prefijo = '') {
 
-inputBio.addEventListener('input', () => {
-    const nuevaBio = inputBio.value.trim();
-    if (nuevaBio) {
-        vistaPreviaBio.textContent = nuevaBio;
-    } else {
-        vistaPreviaBio.textContent = 'Biografía del usuario';
+    const actualizarVistaPrevia = () => {
+        const valor = editarElemento.value.trim();
+        if (valor) {
+            vistaPreviaElemento.textContent = prefijo + valor;
+        } else {
+            vistaPreviaElemento.textContent = prefijo + textoDefault;
+        }
     }
-});
 
-// inputGenero.addEventListener('input', () => {
-//     const nuevoGenero = inputGenero.value.trim();
-//     if (nuevoGenero) {
-//         vistaPreviaGenero.textContent = nuevoGenero;
-//     } else {
-//         vistaPreviaGenero.textContent = 'Género del usuario';
-//     }
-// });
+    actualizarVistaPrevia();
 
-inputUbicacion.addEventListener('input', () => {
-    const nuevaUbicacion = inputUbicacion.value.trim();
-    if (nuevaUbicacion) {
-        vistaPreviaUbicacion.textContent = 'Ciudad: ' + nuevaUbicacion;
-    }
-    else {
-        vistaPreviaUbicacion.textContent = 'Ciudad: ';
-    }
-});
+    editarElemento.addEventListener('input', actualizarVistaPrevia);
+
+}
+
+
+mostrarVistaPrevia(inputNombre, vistaPreviaNombre, 'Nombre Apellido');
+mostrarVistaPrevia(inputBio, vistaPreviaBio, 'Sin biografía');
+//mostrarVistaPrevia(inputGenero, vistaPreviaGenero, '-', 'Género: ');
+mostrarVistaPrevia(inputUbicacion, vistaPreviaUbicacion, '-', 'Ciudad: ');
+mostrarVistaPrevia(inputEdad, vistaPreviaEdad, '-', 'Edad: ');
+mostrarVistaPrevia(inputOrientacion, vistaPreviaOrientacion, '-', 'Orientación sexual: ');
