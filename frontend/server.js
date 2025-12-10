d2e0b019145fac0ebc4671df941e32443cc04bba
tobias <tobiasgabriel.marmolejo@gmail.com>
2025-12-10 20:29:14 -0300

    Agrego server express del frontend

---
+++ frontend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(__dirname));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(8080, () => {
+    console.log('Frontend server is running on http://localhost:8080');
+});
