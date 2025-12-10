ed20ba6876efb21d5717cdb707c163d6f62cc4dd
tobias <tobiasgabriel.marmolejo@gmail.com>
2025-12-10 19:39:28 -0300

    Agrego js para endpoints

--- backend/scripts/index.js
+++ backend/scripts/index.js
@@ -1,9 +1,21 @@
 import express from 'express';
+import { pool } from './db.js';
+
 const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.get('/users', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM users');
+        res.json(result.rows);
+    }   catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
 });
 
 const PORT = process.env.PORT || 3000;
