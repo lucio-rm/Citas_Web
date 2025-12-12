d45ee11c70859240bfd97ac8f0bd45f54561f0dc
tommy <czambrano@fi.uba.ar>
2025-12-12 14:31:54 -0300

    Creo la lógica para que se vean los tags en el editor con un array provisional

--- frontend/js/editar-perfil.js
+++ frontend/js/editar-perfil.js
@@ -1,3 +1,6 @@
+const MAXIMOS= {HOBBIES:5, HABITOS:5, SIGNOS:1, ORIENTACION:1};
+let seleccionados = {HOBBIES:[], HABITOS:[], SIGNOS:[], ORIENTACION:[]};
+
 const inputNombre = document.getElementById('editar-nombre');
 const inputBio = document.getElementById('editar-bio');
 const inputGenero = document.getElementById('editar-genero');
@@ -39,6 +42,48 @@ function mostrarVistaPreviaImagen(editarElemento, vistaPreviaElemento, urlDefaul
 
     editarElemento.addEventListener('input', actualizarVistaPreviaImagen);
 }
+//temporal
+const listaTags = [
+    {id: 1, nombre: 'Futbol', tipo : 'HOBBIES'},
+    {id: 2, nombre: 'Cine', tipo : 'HOBBIES'},
+    {id: 3, nombre: 'Lectura', tipo : 'HOBBIES'},
+    {id: 4, nombre: 'Videojuegos', tipo : 'HOBBIES'},
+    {id: 11, nombre: 'Fumar', tipo : 'HABITOS'},
+    {id: 12, nombre: 'Beber Socialmente', tipo : 'HABITOS'},
+    {id: 13, nombre: 'Tomar café', tipo : 'HABITOS'},
+    {id: 21, nombre: 'Aries', tipo : 'SIGNOS'},
+    {id: 22, nombre: 'Sagitario', tipo : 'SIGNOS'},
+    {id: 31, nombre: 'Heterosexual', tipo : 'ORIENTACION'},
+    {id: 32, nombre: 'Homosexual', tipo : 'ORIENTACION'},
+    {id: 33, nombre: 'Bisexual', tipo : 'ORIENTACION'},
+]
+
+function incializarTags() {
+    const mapeoContenedores = {
+        'HOBBIES' : document.querySelector('.editor-hobbies'),
+        'HABITOS' : document.querySelector('.editor-habitos'),
+        'SIGNOS' : document.querySelector('.editor-signos'),
+        'ORIENTACION' : document.querySelector('.editor-orientacion'),
+    };
+
+    listaTags.forEach(tag => {
+        const contenedor = mapeoContenedores[tag.tipo];
+        if (!contenedor){
+            console.error('ERROR: Tag no encontrado')
+            return;
+        }
+
+        const btn = document.createElement('button');
+        btn.type = 'button';
+        btn.textContent = tag.nombre;
+        btn.className = 'tag tag-btn'
+
+        contenedor.appendChild(btn);
+    })
+}
+
+incializarTags();
+
 
 
 mostrarVistaPrevia(inputNombre, vistaPreviaNombre, 'Nombre Apellido');
