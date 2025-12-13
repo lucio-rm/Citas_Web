43a0cb92756ca69dd813f91902215531ae3ca433
tobias <tobiasgabriel.marmolejo@gmail.com>
2025-12-13 03:38:06 -0300

    Agrego el evento de eliminar el match

--- frontend/js/matches.js
+++ frontend/js/matches.js
@@ -23,7 +23,7 @@ async function mostrarMatches () {
                     <img class="foto" src="https://bulma.io/assets/images/placeholders/96x96.png" alt="icon">
                     <h1 class="nombre letra"></h1>
                 <div>
-                    <button class="color-boton boton">Eliminar</button>
+                    <button class="color-boton boton btn-eliminar">Eliminar</button>
                     <button class="color-boton boton">Organizar evento</button>
                 </div>  
         `;
@@ -38,7 +38,29 @@ async function mostrarMatches () {
         const img = pareja.imagen_url;
         matchActual.querySelector('.nombre').textContent = nombre;
         matchActual.querySelector('.foto').src = img;
+        matchActual.dataset.matchId = match.id;
     };
 }
+
+const contenedorMatches = document.getElementById('contenedor-matches');
+contenedorMatches.addEventListener('click', async (evento) => {
+    if (evento.target.classList.contains('btn-eliminar')) {
+        const matchDiv = evento.target.closest('.contenido');
+        const matchId = matchDiv.dataset.matchId;
+        if (!confirm('¿Estás seguro de qué queres eliminar este match?')) return;
+        try {
+            const response = await fetch(`http://localhost:8080/matches/${matchId}`, {
+                method: 'DELETE'
+            });
+            if (response.ok) {
+                matchDiv.remove();
+            } else {
+                console.error('Error al eliminar el match');
+            }
+        } catch (error) {
+            console.error('Error al eliminar el match:', error);
+        }
+    }
+});
 
 mostrarMatches();
