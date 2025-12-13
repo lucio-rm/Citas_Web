5d89adbe7a230168b0316a50305263e66a5ba1bb
tommy <czambrano@fi.uba.ar>
2025-12-13 17:39:51 -0300

    Creo la lógica para que el boton 'Calificar' abra el modal

--- frontend/js/citas.js
+++ frontend/js/citas.js
@@ -1,7 +1,18 @@
 const btnCerrarModal = document.getElementById('btn-cerrar-modal');
 const modal = document.querySelector('.modal-calificacion-cita');
-const nombrePersonaModal = document.getElementById('modal-nombre-persona');
+const nombreEnModal = document.getElementById('modal-nombre-persona');
 
 btnCerrarModal.addEventListener('click' , () => {
     modal.style.display = 'none';
 });
+
+document.querySelectorAll('.bloque-citas:last-of-type .boton').forEach(boton => {
+    boton.addEventListener('click', (e) => {
+        const tarjeta = e.target.closest('.tarjeta-cita');
+        const nombrePersona = tarjeta.querySelector('h3').innerText;
+        nombreEnModal.innerText = nombrePersona;
+        modal.style.display = 'block';
+
+    });
+});
+
