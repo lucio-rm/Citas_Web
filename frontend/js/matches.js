e65d849a7aaea865d2a7d011679da6b38afe9a1d
tobias <tobiasgabriel.marmolejo@gmail.com>
2025-12-13 02:48:13 -0300

    Hago la funcion para mostrar los matches en el front

--- frontend/js/matches.js
+++ frontend/js/matches.js
@@ -9,3 +9,24 @@ async function cargarMatches () {
         console.error('Error al cargar los matches:', error);
     }
 }
+
+async function mostrarMatches () {
+    const matches = await cargarMatches();
+    matches.forEach(match => {
+        const template = `
+                    <img src="https://bulma.io/assets/images/placeholders/96x96.png" alt="icon">
+                    <h1 class="letra">Nombre y apellido</h1>
+                <div>
+                    <button class="color-boton boton">Eliminar</button>
+                    <button class="color-boton boton">Organizar evento</button>
+                </div>  
+        `;
+        const matchDiv = document.getElementById('contenedor-matches');
+        const matchActual = document.createElement('div');
+        matchActual.classList.add('contenido');
+        matchActual.innerHTML = template;
+        matchDiv.appendChild(matchActual);
+    });
+}
+
+mostrarMatches();
