4f203708d372bb85f86ee891dd14ba537f663fd0
tobias <tobiasgabriel.marmolejo@gmail.com>
2025-12-13 03:07:24 -0300

    Termino la funcion de mostrarMatches y hago una de cargarPersona

--- frontend/js/matches.js
+++ frontend/js/matches.js
@@ -10,12 +10,18 @@ async function cargarMatches () {
     }
 }
 
+async function cargarPersona (id) {
+    const response = await fetch('http://localhost:8080/usuarios/${id}');
+    const pareja = await response.json();
+    return pareja;
+}
+
 async function mostrarMatches () {
     const matches = await cargarMatches();
-    matches.forEach(match => {
+    matches.forEach(async match => {
         const template = `
-                    <img src="https://bulma.io/assets/images/placeholders/96x96.png" alt="icon">
-                    <h1 class="letra">Nombre y apellido</h1>
+                    <img id="foto" src="https://bulma.io/assets/images/placeholders/96x96.png" alt="icon">
+                    <h1 id="nombre" class="letra">Nombre y apellido</h1>
                 <div>
                     <button class="color-boton boton">Eliminar</button>
                     <button class="color-boton boton">Organizar evento</button>
@@ -26,6 +32,14 @@ async function mostrarMatches () {
         matchActual.classList.add('contenido');
         matchActual.innerHTML = template;
         matchDiv.appendChild(matchActual);
+        const id = match.id_usuario_2; // Suponiendo que el id del otro usuario está en id_usuario_2 (Hacer verificación luego)
+        const pareja = await cargarPersona(id);
+        const nombre = pareja.nombre + ' ' + pareja.apellido;
+        const img = pareja.imagen_url;
+        const nombreElemento = getElementById('nombre');
+        const imgElemento = getElementById('foto');
+        nombreElemento.textContent = nombre;
+        imgElemento.src = img;
     });
 }
 
