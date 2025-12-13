c09aca73c93c1384117c73ab8719a4090eef34bd
tommy <czambrano@fi.uba.ar>
2025-12-13 18:33:54 -0300

    Agregué lógica para guardar los datos del formulario, temporalmente en un diccionario

--- frontend/js/citas.js
+++ frontend/js/citas.js
@@ -1,6 +1,7 @@
 const btnCerrarModal = document.getElementById('btn-cerrar-modal');
 const modal = document.querySelector('.modal-calificacion-cita');
 const nombreEnModal = document.getElementById('modal-nombre-persona');
+const formulario = document.querySelector('.calificar-cita-form')
 
 btnCerrarModal.addEventListener('click' , () => {
     modal.style.display = 'none';
@@ -16,3 +17,25 @@ document.querySelectorAll('.bloque-citas:last-of-type .boton').forEach(boton => 
     });
 });
 
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const puntajes = formulario.querySelectorAll('input[type="number"]');
+    const repiteSeleccionado = formulario.querySelector('input[type="radio"]:checked');
+    const notaExtra = formulario.querySelector('textarea').value;
+
+    const calificaciones = {
+        evento : puntajes[0].value,
+        pareja : puntajes[1].value,
+        puntualidad : puntajes[2].value,
+        fluidez_conexion : puntajes[3].value,
+        comodidad : puntajes[4].value,
+        calidad_evento : puntajes[5].value,
+        repitirias : repiteSeleccionado ? repiteSeleccionado.value : 'no',
+        nota_extra : notaExtra
+    }
+    console.log(calificaciones)
+    
+    modal.style.display = 'none';
+    alert('Calificación guardada');
+})
